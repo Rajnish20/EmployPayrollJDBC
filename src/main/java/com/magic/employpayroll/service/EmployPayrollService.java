24ba17067a1359de7cf86e4ce16e7c28f7ca8a6f
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-06-20 15:54:15 +0530

    Add-display welcome message

---
+++ src/main/java/com/magic/employpayroll/service/EmployPayrollService.java
@@ -0,0 +1,15 @@
+package com.magic.employpayroll.service;
+
+import com.magic.employpayroll.entity.Employ;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployPayrollService {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employ Payroll");
+    }
+}
---
+++ src/test/java/com/magic/employpayroll/PayrollServiceTest.java
@@ -0,0 +1,13 @@
+package com.magic.employpayroll;
+
+import com.magic.employpayroll.entity.Employ;
+import com.magic.employpayroll.service.EmployPayrollService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class PayrollServiceTest {
+
+
+}
